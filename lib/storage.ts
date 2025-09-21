f0e7cfc0655c2b760f553efc3fb272a643b49f6d
kintuhq <109710137+kintuhq@users.noreply.github.com>
2025-09-21 14:06:26 +0700

    3

--- lib/storage.ts
+++ lib/storage.ts
@@ -4,6 +4,7 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 const s3Client = new S3Client({
   region: 'auto',
   endpoint: process.env.R2_ENDPOINT,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
